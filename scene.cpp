c68c84d985c4282f9b8c446b7340a3dce3de572c
OakenMan <tom.suchel@gmail.com>
2020-05-16 17:47:29 +0200

    commentaires mouvement

--- scene.cpp
+++ scene.cpp
@@ -69,6 +69,9 @@ void Scene::setJumpFactor(const qreal &newJumpFactor)
     emit jumpFactorChanged(jumpFactor);
 }
 
+/**
+ * @brief Event déclanché à chaque clic de souris (actuellement utilisé pour placer des blocs, à supprimer plus tard)
+ */
 void Scene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
     if(event->button() == Qt::RightButton) {
@@ -83,34 +86,40 @@ void Scene::mousePressEvent(QGraphicsSceneMouseEvent *event)
     QGraphicsScene::mousePressEvent(event);
 }
 
+/**
+ * @brief Filtre d'évenements. Récupère les touches pressées et relachées pour gérer les mouvements du joueur
+ */
 bool Scene::eventFilter(QObject *watched, QEvent *event)
 {
     Q_UNUSED(watched);
+
+    // Si une touche est pressée
     if(event->type() == QEvent::KeyPress) {
-        if(((QKeyEvent*)event)->isAutoRepeat()) {
+        if(((QKeyEvent*)event)->isAutoRepeat()) {   // On ignore les event à part le tout premier
             return false;
         }
         int key = ((QKeyEvent*)event)->key();
         switch(key) {
-        case Qt::Key_D:
+        case Qt::Key_D:                     // Droite
             player->addDirection(1);
             checkTimer();
             break;
-        case Qt::Key_Q:
+        case Qt::Key_Q:                     // Gauche
             player->addDirection(-1);
             checkTimer();
             break;
-        case Qt::Key_Space:
-            if(fallTimer->isActive()) {
+        case Qt::Key_Space:                 // Saut
+            if(fallTimer->isActive()) {     // On ne peut pas sauter si on tombe
                 return false;
             }
             else {
                 if(jumpAnimation->state() == QAbstractAnimation::Stopped)
-                jumpAnimation->start();
+                    jumpAnimation->start();
             }
             break;
         }
     }
+    // Si une touche est relachée, on stop le mouvement
     else if(event->type() == QEvent::KeyRelease) {
         if(((QKeyEvent*)event)->isAutoRepeat()) {
             return false;
@@ -130,11 +139,15 @@ bool Scene::eventFilter(QObject *watched, QEvent *event)
     return false;
 }
 
+/**
+ * @brief Fonction appelée par le timer 'moveTimer' lorsque le joueur est en mouvement
+ */
 void Scene::movePlayer() {
 
-//    if(player->isFalling()) {
-//        return;
-//    }
+    // Dé-commenter pour interdire le mouvement en chute libre
+    //    if(player->isFalling()) {
+    //        return;
+    //    }
 
     int direction = player->getDirection();
 
@@ -142,9 +155,12 @@ void Scene::movePlayer() {
         return;
     }
 
+    // Si le joueur n'est pas en train de tomber
     if(!player->isFalling()) {
+        // Qu'il ne touche aucune plateforme et qu'il n'est pas en train de sauter
         if(!(lastPlatform && player->isTouchingPlatform(lastPlatform)) && jumpAnimation->state() == QAbstractAnimation::Stopped) {
             if(lastPlatform) {
+                // Alors il tombe
                 player->fall();
                 fallTimer->start();
             }
@@ -156,7 +172,7 @@ void Scene::movePlayer() {
     // Si il va à droite
     if(direction > 0) {
 
-        // On bloque tout mouvement au delà de 7950
+        // On bloque tout mouvement au delà de 7950 (frontière droite)
         if(player->pos().x() > 7950) {
             return;
         }
@@ -187,48 +203,51 @@ void Scene::movePlayer() {
         player->moveBy(dx, 0);
         int diff = player->pos().x() - scroll->value();
         if(diff < 480) {
-            if(scroll->value() < 100) {
+            if(scroll->value() < 0) {
                 return;
             }
             scroll->setValue(scroll->value() + dx);
-            // Ici, déplacer aussi tous les éléments de l'UI de dx
+            // [!] Ici, déplacer aussi tous les éléments de l'UI de dx
         }
     }
-
-
 
     return;
 }
 
+/**
+ * Fonction appelée lorsque le 'jumpfactor' change, donc au fur et à mesure que l'animation de jump évolue
+ */
 void Scene::jumpPlayer() {
 
+    // Si l'animation est finie, on s'arrête là
     if(jumpAnimation->state() == QAbstractAnimation::Stopped) {
-        qDebug() << "animation finie, on stop";
         player->stand();
         return;
     }
 
+    // Si le joueur touchr une plateforme
     QGraphicsItem *item = collidingPlatforms();
     if(item) {
+        // Avec la tête...
         if(player->isTouchingHead(item)) {
 
-            qDebug() << "head touched";
+            jumpAnimation->stop();  // On stoppe l'animation (TODO: le faire redescendre plus doucement ?)
 
-            jumpAnimation->stop();
-
+            // Puis on le fait retomber sur sa dernière plateforme
             if(lastPlatform) {
                 player->setPos(player->pos().x(), lastPlatform->pos().y() - player->boundingRect().height());
                 return;
             }
+            // Ou le sol
             if(!lastPlatform) {
                 player->setPos(player->pos().x(), groundLevel - player->boundingRect().height());
                 return;
             }
         }
+        // Avec ses pieds...
         else {
             if(handleCollisionWithPlatform()) {
-                qDebug() << "platform touched";
-                return;
+                return; // On s'arrête là
             }
         }
     }
@@ -236,9 +255,10 @@ void Scene::jumpPlayer() {
     if(fallTimer->isActive()) {
         return;
     }
+
+    // y = future position Y du joueur
     qreal y = (groundLevel - player->boundingRect().height()) - jumpAnimation->currentValue().toReal() * jumpHeight;
     if(lastPlatform) {
-        qDebug() << "last platform = " << lastPlatform;
         y = (lastPlatform->pos().y() - player->boundingRect().height()) - jumpAnimation->currentValue().toReal() * jumpHeight;
         if(!(lastPlatform && player->isTouchingPlatform(lastPlatform)) && jumpFactor < 0.1) {
             if((player->pos().x() < lastPlatform->pos().x()) || (player->pos().x() > lastPlatform->pos().x() + lastPlatform->boundingRect().width())) {
@@ -253,53 +273,78 @@ void Scene::jumpPlayer() {
             }
         }
     }
+
+    // On met à jour la position du joueur
     player->setPos(player->pos().x(), y);
 }
 
+
+/**
+ * Fonction appelée par le timer 'fallTimer', lorsque le joueur est en train de tomber
+ */
 void Scene::fallPlayer() {
-    qDebug() << "fall";
+    // On met à jour la position du joueur (TODO : gérer l'acceleration ?)
     player->setPos(player->pos().x(), player->pos().y() + 20);
+
     QGraphicsItem *item = collidingPlatforms();
+
+    // Si il touche une plateforme, on arrête la chute
     if(item && handleCollisionWithPlatform()) {
         fallTimer->stop();
         player->walk();
     }
-        else if(player->pos().y() + player->boundingRect().height() >= groundLevel) {
-            player->setPos(player->pos().x(), groundLevel - player->boundingRect().height());
-            fallTimer->stop();
-            player->walk();
-            lastPlatform = 0;
-        }
+    // Si il arrive au sol, on arrête la chute
+    else if(player->pos().y() + player->boundingRect().height() >= groundLevel) {
+        player->setPos(player->pos().x(), groundLevel - player->boundingRect().height());
+        fallTimer->stop();
+        player->walk();
+        lastPlatform = 0;
+    }
 }
 
+/**
+ * Fonction appelée à chaque input, pour vérifier si le joueur est encore en mouvement ou pas
+ */
 void Scene::checkTimer() {
+    // Si il ne bouge pas, on arrête le timer
     if(player->getDirection() == 0) {
         player->stand();
         moveTimer->stop();
     }
+    // Si le timer était éteind, on le redémarre
     else if(!moveTimer->isActive()) {
         moveTimer->start();
         player->walk();
     }
 }
 
+/**
+ * @brief Renvoie le RigidBody touché par le joueur
+ */
 QGraphicsItem * Scene::collidingPlatforms() {
     QList<QGraphicsItem*> items = collidingItems(player);
     foreach(QGraphicsItem *item, items) {
         if(RigidBody *rigidbody = qgraphicsitem_cast<RigidBody *>(item)) {
             return rigidbody;
         }
+//        if(AutreObjet *other = qgraphicsitem_cast<AutreObjet *>(item)) {
+//            return other;
+//        }
     }
     return 0;
 }
 
+// ça maybe on s'en tape
 void Scene::jumpStatusChanged(QAbstractAnimation::State newState, QAbstractAnimation::State oldState)
 {
     if(newState == QAbstractAnimation::Stopped && oldState == QAbstractAnimation::Running) {
-//         handleCollisionWithPlatform(); //??
+        //         handleCollisionWithPlatform(); //??
     }
 }
 
+/**
+ * Gère les collision avec les plateformes (verticales seulement)
+ */
 bool Scene::handleCollisionWithPlatform() {
     QGraphicsItem * platform = collidingPlatforms();
     if(platform) {
